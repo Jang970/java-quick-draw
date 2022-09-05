0e0196110a60a62998138db00852ea4c98c554af
jang970 <jang970@aucklanduni.ac.nz>
2022-09-05 22:37:11 +1200

    created new class, User and provided small class description about the purpose of the class

---
+++ src/main/java/nz/ac/auckland/se206/util/User.java
@@ -0,0 +1,9 @@
+package nz.ac.auckland.se206.util;
+
+/**
+ * This class will be used to create objects for each new user profile containing all information related to that specific profile.
+ * We will be using the concept of json serialisation and de-serialisation in order to save and load exisiting user profiles in our app.
+ */
+public class User {
+  
+}
