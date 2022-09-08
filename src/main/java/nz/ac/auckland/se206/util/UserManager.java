9db86d978e13ee9fc399ce034351c2dc25762b57
jang970 <jang970@aucklanduni.ac.nz>
2022-09-08 23:30:50 +1200

    added method getExistingProfiles that will return a list of User class objects from json file

--- src/main/java/nz/ac/auckland/se206/util/UserManager.java
+++ src/main/java/nz/ac/auckland/se206/util/UserManager.java
@@ -2,12 +2,17 @@ package nz.ac.auckland.se206.util;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.Writer;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * This class will be used to handle any functionalities we would like to do with User class objects/instances
@@ -36,6 +41,31 @@ public class UserManager {
       gson.toJson(users, writer);
     }
 
+  }
+
+  /**
+   * Call this method when you want a list of all User objects that have been created and stored on json file
+   * Can only call if the json file already exists
+   * @return arraylist of User objects
+   * @throws IOException
+   */
+  public List<User> getExistingProfiles() throws IOException{
+
+    // deserialising file containing existing profiles
+    // de-serialisation
+    // create file reader
+    Reader reader = Files.newBufferedReader(Paths.get("UserProfiles.json"));
+
+    Gson gson = new Gson();
+    Type listType = new TypeToken<ArrayList<User>>(){}.getType();
+        
+    // converting json string to arraylist of user objects
+    List<User> listOfUsers = gson.fromJson(reader, listType);
+
+    // close reader
+    reader.close();
+
+    return listOfUsers;
   }
 
 }
