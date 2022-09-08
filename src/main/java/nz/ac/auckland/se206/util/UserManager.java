af00c380bdc9b28017b846796fa21c99e37caed0
jang970 <jang970@aucklanduni.ac.nz>
2022-09-08 22:42:03 +1200

    created new class UserManager that will contain methods relating to User class instances

---
+++ src/main/java/nz/ac/auckland/se206/util/UserManager.java
@@ -0,0 +1,9 @@
+package nz.ac.auckland.se206.util;
+
+/**
+ * This class will be used to handle any functionalities we would like to do with User class objects/instances
+ * An example would be saving a newly created user profile into our json file
+ */
+public class UserManager {
+  
+}
