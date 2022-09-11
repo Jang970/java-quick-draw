fa21d976b8e09a669eac42f22a7d9891da8f929e
PK Wadsworth <kwad109@aucklanduni.ac.nz>
2022-09-11 12:49:56 +1200

    Moved DataSource into Utils

--- src/main/java/nz/ac/auckland/se206/util/DataSource.java
+++ src/main/java/nz/ac/auckland/se206/util/DataSource.java
@@ -1,4 +1,4 @@
-package nz.ac.auckland.se206;
+package nz.ac.auckland.se206.util;
 
 /** A class complying with this ineterface should provide a method which provides an image */
 public interface DataSource<Data> {
