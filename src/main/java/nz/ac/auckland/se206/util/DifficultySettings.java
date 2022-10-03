cdca8c735b8d087e68573be58286a5ee15c39d08
jang970 <jang970@aucklanduni.ac.nz>
2022-10-03 22:10:47 +1300

    Forgot to save! Class set up is now saved

--- src/main/java/nz/ac/auckland/se206/util/DifficultySettings.java
+++ src/main/java/nz/ac/auckland/se206/util/DifficultySettings.java
@@ -1,1 +1,18 @@
+package nz.ac.auckland.se206.util;
 
+import nz.ac.auckland.se206.GameLogicManager.CategoryType;
+
+/**
+ * This class will house the difficulty settings for a given profile This will be stored within the
+ * Profile class It will store the difficulties set for Accuracy, Words, Time & Confidence
+ */
+public class DifficultySettings {
+
+  // instance fields, we will have default difficulty values for each setting
+  private int topNumGuesses = 3;
+  private int timeToDraw = 60;
+  private double confidenceLevel = 1;
+  private CategoryType wordCategory = CategoryType.EASY;
+
+  public DifficultySettings() {}
+}
