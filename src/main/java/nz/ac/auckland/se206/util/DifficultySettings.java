27e3a3ed6e837308ce033321a83bc827b3fd1710
jang970 <jang970@aucklanduni.ac.nz>
2022-10-03 22:24:29 +1300

    Created getters and setters for instance fields

--- src/main/java/nz/ac/auckland/se206/util/DifficultySettings.java
+++ src/main/java/nz/ac/auckland/se206/util/DifficultySettings.java
@@ -9,10 +9,43 @@ import nz.ac.auckland.se206.GameLogicManager.CategoryType;
 public class DifficultySettings {
 
   // instance fields, we will have default difficulty values for each setting
-  private int topNumGuesses = 3;
+  private int accuracyLevel = 3;
   private int timeToDraw = 60;
   private double confidenceLevel = 1;
   private CategoryType wordCategory = CategoryType.EASY;
 
   public DifficultySettings() {}
+
+  // getters and setters for each instance field
+  public int getAccuracy() {
+    return this.accuracyLevel;
+  }
+
+  public int getTimeToDraw() {
+    return this.timeToDraw;
+  }
+
+  public double getConfidence() {
+    return this.confidenceLevel;
+  }
+
+  public CategoryType getCategoryOfWords() {
+    return this.wordCategory;
+  }
+
+  public void changeAccuracyLevel(int newAccuracy) {
+    this.accuracyLevel = newAccuracy;
+  }
+
+  public void changeTimeToDraw(int newTime) {
+    this.timeToDraw = newTime;
+  }
+
+  public void changeConfidenceLevel(double newConfidence) {
+    this.confidenceLevel = newConfidence;
+  }
+
+  public void changeCategoryOfWords(CategoryType category) {
+    this.wordCategory = category;
+  }
 }
