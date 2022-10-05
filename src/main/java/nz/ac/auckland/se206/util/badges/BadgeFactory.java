54795f29592eff9a22a10c58e3272c409b032a8f
jang970 <jang970@aucklanduni.ac.nz>
2022-10-06 01:52:45 +1300

    implemented factory class for badges

--- src/main/java/nz/ac/auckland/se206/util/badges/BadgeFactory.java
+++ src/main/java/nz/ac/auckland/se206/util/badges/BadgeFactory.java
@@ -1,3 +1,31 @@
 package nz.ac.auckland.se206.util.badges;
 
-public class BadgeFactory {}
+import nz.ac.auckland.se206.GameLogicManager.GameEndInfo;
+
+/** Making use of Factory design pattern to handle creation of badges */
+public class BadgeFactory {
+
+  public static Badge createBadge(int type, GameEndInfo gameInfo) {
+
+    switch (type) {
+      case 1:
+        return new TimeBadge(gameInfo);
+
+      case 2:
+        return new AllCategoriesBadge(gameInfo);
+
+      case 3:
+        return new MaxDifficultyBadge(gameInfo);
+
+      case 4:
+        return new TwoSecondsLeftBadge(gameInfo);
+
+      case 5:
+        return new CompleteGameBadge(gameInfo);
+
+      default:
+        System.out.println("Error! No Badge of that type found");
+        return null;
+    }
+  }
+}
